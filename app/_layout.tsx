7bb472db9822e08750e70fe4708bcd9905f2b833
dilnawaz khan <dilnawaz@protogoup.co>
2024-08-14 00:23:01 +0500

    login and sign up UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,20 @@
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Link, Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import "react-native-reanimated";
 import "expo-dev-client";
+import Colors from "@/constants/Colors";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+function InitialLayout() {
+  const router = useRouter();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -26,6 +32,67 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="sign-up"
+        options={{
+          title: "",
+          headerBackTitle: "",
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: Colors.background,
+          },
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="login"
+        options={{
+          title: "",
+          headerBackTitle: "",
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: Colors.background,
+          },
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <Link href={"/help"} asChild>
+              <TouchableOpacity>
+                <Ionicons
+                  name="help-circle-outline"
+                  size={34}
+                  color={Colors.dark}
+                />
+              </TouchableOpacity>
+            </Link>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="help"
+        options={{
+          title: "Help",
+          presentation: "modal",
+        }}
+      />
     </Stack>
   );
 }
+
+const RootLayoutNav = () => {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <InitialLayout />
+    </GestureHandlerRootView>
+  );
+};
+
+export default RootLayoutNav;
--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,11 +1,94 @@
-import { View, Text } from "react-native";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { Link } from "expo-router";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 const SignUp = () => {
+  const [countryCode, setCountryCode] = useState("+92");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const keyboardVerticalOffset = Platform.OS === "ios" ? 80 : 0;
+  const onSignUp = async () => {};
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior="padding"
+      keyboardVerticalOffset={keyboardVerticalOffset}
+    >
+      <View style={defaultStyles.container}>
+        <Text style={defaultStyles.header}>Let's ge started!</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter your phone number, we will send you a confirmation code there
+        </Text>
+        <View style={styles.inputContianer}>
+          <TextInput
+            placeholder="Country code"
+            keyboardType="number-pad"
+            value={countryCode}
+            placeholderTextColor={Colors.gray}
+            style={styles.input}
+          />
+          <TextInput
+            placeholder="Mobile number"
+            placeholderTextColor={Colors.gray}
+            keyboardType="number-pad"
+            style={[styles.input, { flex: 1 }]}
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+        </View>
+        <Link asChild href={"/login"} replace>
+          <TouchableOpacity>
+            <Text style={defaultStyles.textLink}>
+              Already have an account? Login
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <View style={{ flex: 1 }} />
+        <TouchableOpacity
+          onPress={onSignUp}
+          style={[
+            defaultStyles.pillButton,
+            phoneNumber !== "" ? styles.enabled : styles.disabled,
+            { marginBottom: 20 },
+          ]}
+        >
+          <Text style={defaultStyles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default SignUp;
+
+const styles = StyleSheet.create({
+  inputContianer: {
+    marginVertical: 40,
+    flexDirection: "row",
+    gap: 10,
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
